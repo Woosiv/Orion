8ebeb1dd929639b6512297bc414ec11968471692
RyanLam2000 <rlam10@uci.edu>
2021-11-27 20:01:55 -0800

    Added transitions and animations between scenes (loading scene broken)

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -4,33 +4,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public static class Loader
+public class Loader : MonoBehaviour
 {
-    public enum Scene {
-        MainMenu,
-        Scorpius,
-        Loading,
-        LevelSelect
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    public void LoadLevel(String scene)
+    {
+        // Debug.Log("Starts the loading");
+        // SceneManager.LoadScene("Loading");
+        // StartCoroutine(LoadLoading());
+        
+        // Debug.Log("Starts the level");
+        StartCoroutine(LoadAsynchronously(scene));
     }
 
-    private static Action onLoaderCallback;
-
-    public static void Load(Scene scene)
+    IEnumerator LoadEverything(String scene)
     {
-        onLoaderCallback = () =>
-        {
-            SceneManager.LoadScene(scene.ToString());
-        };
+        Debug.Log("Starts the loading");
+        yield return StartCoroutine(LoadLoading());
 
-        SceneManager.LoadScene(Scene.Loading.ToString());
+        Debug.Log("Starts loading the level");
+        yield return StartCoroutine(LoadAsynchronously(scene));
     }
 
-    public static void LoaderCallback()
+    IEnumerator LoadLoading()
     {
-        if (onLoaderCallback != null)
+        // Starts the animation
+        transition.SetTrigger("Start");
+
+        // Waits for the set transition time
+        yield return new WaitForSeconds(transitionTime);
+
+        // Loads the loading screen
+        SceneManager.LoadScene("Loading");
+    }
+
+    IEnumerator LoadAsynchronously(String scene)
+    {
+        Debug.Log("It reached here");
+        // Starts the animation
+        transition.SetTrigger("Start");
+
+        Debug.Log(transitionTime);
+        // Waits for the set transition time
+        yield return new WaitForSeconds(transitionTime);
+
+        Debug.Log("Made it past yield");
+        // Starts loading the next scene
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+        Debug.Log(operation.isDone);
+        Debug.Log(operation.progress);
+
+        // Waits for operation to complete
+        while (!operation.isDone)
         {
-            onLoaderCallback();
-            onLoaderCallback = null;
+            // Debug.Log(operation.progress);
+            yield return null;
         }
+
+        // if (operation.isDone)
+        // {
+        //     Debug.Log("It finished");
+        // }
     }
 }
